39513d7039a6e8c2cd2c7a0195205c6ba45c84e0
NigrumAccipiter <ottoarturomadriz@gmail.com>
2026-07-06 16:44:38 -0400

    -se ha añadido un tileMap y su respectivo código para leerlo. -Se añadieron nuevos tiles
    Modificaciones en: TileManager ; map01.txt

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -4,17 +4,25 @@ import main.GamePanel;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class TileManager {
     GamePanel gp;
     Tile[] tile;
+    int mapTileNum [][];
 
     public TileManager(GamePanel gp) {
 
         this.gp = gp;
         tile = new Tile[10];
+
+        mapTileNum = new int[gp.maxScreenCol][gp.maxScreenRow];
+
         getTileImage();
+        loadMap();
     }
 
     public void getTileImage() {
@@ -22,11 +30,20 @@ public class TileManager {
         try {
 
             tile[0] = new Tile();
-            tile[0].image = ImageIO.read(getClass().getResourceAsStream("/tiles/agua.png"));
+            tile[0].image = ImageIO.read(getClass().getResourceAsStream("/tiles/ceniza.png"));
             tile[1] = new Tile();
             tile[1].image = ImageIO.read(getClass().getResourceAsStream("/tiles/agua.png"));
             tile[2] = new Tile();
-            tile[2].image = ImageIO.read(getClass().getResourceAsStream("/tiles/pisoCeniza.png"));
+            tile[2].image = ImageIO.read(getClass().getResourceAsStream("/tiles/CenizaHumeda1.png"));
+            tile[3] = new Tile();
+            tile[3].image = ImageIO.read(getClass().getResourceAsStream("/tiles/CenizaHumedaEsquina1.png"));
+            tile[4] = new Tile();
+            tile[4].image = ImageIO.read(getClass().getResourceAsStream("/tiles/CenizaHumedaEsquina2.png"));
+            tile[5] = new Tile();
+            tile[5].image = ImageIO.read(getClass().getResourceAsStream("/tiles/PisoCeniza.png"));
+            tile[6] = new Tile();
+            tile[6].image = ImageIO.read(getClass().getResourceAsStream("/tiles/aguaCeniza.png"));
+
 
 
         }catch(IOException e) {
@@ -35,22 +52,61 @@ public class TileManager {
 
     }
 
+    public void loadMap(){
+
+        try{
+            InputStream is = getClass().getResourceAsStream("/maps/map01.txt");
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+            int col = 0;
+            int row = 0;
+
+            while (col < gp.maxScreenCol && row < gp.maxScreenRow) {
+
+                String line = br.readLine();
+
+                while (col < gp.maxScreenCol) {
+                    String numbers[] = line.split(" ");
+                    int num = Integer.parseInt(numbers[col]);
+
+                    mapTileNum[col][row] = num;
+                    col++;
+                }
+
+                if (col == gp.maxScreenCol) {
+
+                    col = 0;
+                    row++;
+                }
+            }
+            br.close();
+
+        } catch (Exception e) {
+
+        }
+
+
+    }
+
+
     public void draw(Graphics2D g2) {
 
-        int col1 = 0;
+        int col = 0;
         int row = 0;
 
         int x= 0;
         int y= 0;
 
-        while (col1< gp.maxScreenCol && row< gp.maxScreenRow){
+        while (col< gp.maxScreenCol && row< gp.maxScreenRow){
 
-            g2.drawImage(tile[0].image, x, y, gp.tileSize, gp.tileSize, null);
-            col1++;
+            int tileNum =  mapTileNum[col][row];
+
+            g2.drawImage(tile[tileNum].image, x, y, gp.tileSize, gp.tileSize, null);
+            col++;
             x += gp.tileSize;
 
-            if(col1 == gp.maxScreenCol){
-                col1 = 0;
+            if(col == gp.maxScreenCol){
+                col = 0;
                 x = 0;
                 row++;
 
